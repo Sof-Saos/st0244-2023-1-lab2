3d1192654792585dfc0e396a9f8d1c248486aa8b
Sofía Salamanca <74073693+Sof-Saos@users.noreply.github.com>
2023-05-04 23:13:37 -0500

    Update Tests.hs

--- src/Tests.hs
+++ src/Tests.hs
@@ -1,1 +1,12 @@
+import PRF(f1, f2, f3, f4, f5)
 
+
+
+
+--main :: IO()
+--main = do
+--  quickCheck prop_f1
+--  quickCheck prop_f2
+--  quickCheck prop_f3
+-- quickCheck prop_f4
+--quickCheck prop_f5
