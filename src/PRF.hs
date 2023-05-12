499c2c6b476909292a377a95835f75e0a2692233
Sofía Salamanca <74073693+Sof-Saos@users.noreply.github.com>
2023-05-12 17:05:33 -0500

    Add files via upload

--- src/PRF.hs
+++ src/PRF.hs
@@ -1,13 +1,57 @@
-module PFR (f1, f2, f3, f4, f5) where
+module PRF  where
+import Prelude hiding (toInteger)
+
 data Nat = Zero | Succ Nat
+
+instance Eq Nat where
+  Zero == Zero         = True
+  Succ n == Succ m     = n == m
+  _      == _          = False
+instance Num Nat where
+  fromInteger :: Integer -> Nat
+  fromInteger n
+    | n <= 0    = Zero
+    | otherwise = Succ (fromInteger (n - 1))
+
+  (+) :: Nat -> Nat -> Nat
+  (+) = f1
+
+  (*) :: Nat -> Nat -> Nat
+  (*) = f2
+
+  abs :: Nat -> Nat
+  abs n = n
+
+  signum :: Nat -> Nat
+  signum Zero   = Zero
+  signum Succ{} = Succ Zero
+
+  negate :: Nat -> Nat
+  negate n = n  
+
+--------------  Funciones -------------
+f1 :: Nat -> Nat -> Nat --Suma
+f1 m n = recNat n (\ _ y -> Succ y) m
+
+f2 :: Nat -> Nat -> Nat --Multiplicación
+f2 m = recNat Zero (\n acc -> f2 acc m)
+
+f3 :: Nat -> Nat -> Nat --Potencia
+f3 m n = recNat (Succ Zero) (\_ y -> f2 m y) n
+
+-- f4 :: 
+-- f4 m
+
+-- f5 :: 
+-- f5 
 
 recNat :: a -> (Nat -> a -> a) -> Nat -> a
 recNat a _ Zero = a
 recNat a h (Succ n) = h n (recNat a h n)
 
-f1 :: Nat -> Nat -> Nat
-f1 m n = recNat n (\ _ y -> Succ y) m
+toInteger :: Nat -> Integer
+toInteger = recNat 0 (\_ acc -> acc + 1)
 
-f2 :: Nat -> Nat -> Nat
-f2 m = recNat Zero (\n acc -> f2 acc m)
+instance Show Nat where
+  show n = show (toInteger n)
 
