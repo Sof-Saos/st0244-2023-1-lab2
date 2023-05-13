e5c90f22ade5b35fb9c981a7da668a84f17156d7
Sofía Salamanca <74073693+Sof-Saos@users.noreply.github.com>
2023-05-13 00:09:08 -0500

    Updates and documentation

--- src/PRF.hs
+++ src/PRF.hs
@@ -5,24 +5,25 @@ import Test.QuickCheck (Arbitrary, arbitrary, oneof, sized, shrink, Gen)
 
 data Nat = Zero | Succ Nat
 
-----Instancias 
-instance Eq Nat where
+----Instances
+instance Eq Nat where --Compare Nats
    Zero == Zero         = True
    Succ n == Succ m     = n == m
    _      == _          = False
-instance Num Nat where
+instance Num Nat where --Define basics algebraic operations
   fromInteger :: Integer -> Nat
   fromInteger n
     | n <= 0    = Zero
     | otherwise = Succ (fromInteger (n - 1))
 
-  (+) :: Nat -> Nat -> Nat
+  (+) :: Nat -> Nat -> Nat --Define Sum operators
   (+) = f1
 
-  (*) :: Nat -> Nat -> Nat
+  (*) :: Nat -> Nat -> Nat --Define mult operators
   (*) = f2
-
-  abs :: Nat -> Nat
+  
+  --required syntax of the instance
+  abs :: Nat -> Nat 
   abs n = n
 
   signum :: Nat -> Nat
@@ -31,38 +32,41 @@ instance Num Nat where
 
   negate :: Nat -> Nat
   negate n = n  
-instance Arbitrary Nat where
-  arbitrary = sized $ \n -> genNat n
+instance Arbitrary Nat where --Generate random values
+  arbitrary = sized $ \n -> genNat n --random generation of values depending on n
     where
-      genNat :: Int -> Gen Nat
+      genNat :: Int -> Gen Nat --Funtion that generate a random value of Nat
       genNat 0 = return Zero
       genNat n = oneof [return Zero, Succ <$> genNat (n - 1)]
-  shrink Zero     = []
-  shrink (Succ n) = n : shrink n
-instance Ord Nat where
+  shrink Zero     = [] --No values are generated because there are no values smaller than zero
+  --If the value generated is Succ then it generates smaller values than the previous one
+  shrink (Succ n) = n : shrink n 
+instance Ord Nat where -- Compare Nats values to order them
   Zero <= _ = True
   Succ m <= Succ n = m <= n
   _ <= _ = False
-instance Show Nat where
+instance Show Nat where --Converts to strings
   show n = show (toInteger n)
-----Funciones
-f1 :: Nat -> Nat -> Nat --Suma
+----Funtiones
+f1 :: Nat -> Nat -> Nat -- Sum
 f1 m n = recNat n (\ _ y -> Succ y) m
 
-f2 :: Nat -> Nat -> Nat --Multiplicación
+f2 :: Nat -> Nat -> Nat --Multiplication
 f2 m = recNat Zero (\_ acc -> f2 acc m)
-f3 :: Nat -> Nat -> Nat --Potencia
+
+f3 :: Nat -> Nat -> Nat --Pow
 f3 m n = recNat (Succ Zero) (\_ y -> f2 m y) n
 
-f4 :: Nat -> Nat -> Nat -- Suma recursiva que utiliza f1
+f4 :: Nat -> Nat -> Nat -- Sum using the result of f1
 f4 m n = recNat n (\ _ acc -> f1 m acc) m
 
--- f5 :: Nat -> Nat -> Nat
--- f5 
+f5 :: Nat -> Nat -> Nat --Id
+f5 m n = recNat m (\_ acc -> Succ acc) n
 
 recNat :: a -> (Nat -> a -> a) -> Nat -> a
 recNat a _ Zero = a
 recNat a h (Succ n) = h n (recNat a h n)
-toInteger :: Nat -> Integer
+
+toInteger :: Nat -> Integer --Converts to Integer
 toInteger = recNat 0 (\_ acc -> acc + 1)
 
