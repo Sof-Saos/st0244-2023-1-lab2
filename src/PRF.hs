376b981a4927a8173e6d5c790ddc4c0b4cb0b062
Sofía Salamanca <74073693+Sof-Saos@users.noreply.github.com>
2023-05-15 22:02:09 -0500

    code file update
    - correction and removing of functions that did not work or didn't work as expected
    - Addition of instances for the Natural datatype
    - Addition of functions naturalToNat and natToNatural

--- src/PRF.hs
+++ src/PRF.hs
@@ -3,22 +3,55 @@ import Prelude hiding (toInteger)
 import Test.QuickCheck (Arbitrary, arbitrary, oneof, sized, shrink, Gen)
 
 data Nat = Zero | Succ Nat
+data Natural = NatZero | NatSucc Natural
 ----Instances
-instance Eq Nat where --Compare Nats
-   Zero == Zero         = True
-   Succ n == Succ m     = n == m
-   _      == _          = False
+instance Eq Natural where
+  (==) :: Natural -> Natural -> Bool
+  NatZero == NatZero = True
+  NatSucc n == NatSucc m = n == m
+  _ == _ = False
+instance Num Natural where
+  fromInteger :: Integer -> Natural
+  fromInteger n
+    | n <= 0 = NatZero
+    | otherwise = NatSucc (fromInteger (n - 1))
+
+  (+) :: Natural -> Natural -> Natural
+  NatZero + n = n
+  NatSucc m + n = NatSucc (m + n)
+
+  (*) :: Natural -> Natural -> Natural
+  NatZero * _ = NatZero
+  NatSucc m * n = n + (m * n)
+
+  abs :: Natural -> Natural
+  abs n = n
+
+  signum :: Natural -> Natural
+  signum NatZero = NatZero
+  signum _ = NatSucc NatZero
+
+  negate :: Natural -> Natural
+  negate n = n
+
+(^) :: Natural -> Natural -> Natural --Define a pow operator to be used as a comparator in the prop
+m ^ n = natToNatural (f3 m' n')
+  where
+      m', n' :: Nat
+      m' = naturalToNat m
+      n' = naturalToNat n
+
 instance Num Nat where --Define basics algebraic operations
   fromInteger :: Integer -> Nat
   fromInteger n
     | n <= 0    = Zero
     | otherwise = Succ (fromInteger (n - 1))
-  (+) :: Nat -> Nat -> Nat --Define Sum operators
+  (+) :: Nat -> Nat -> Nat --Define Sum operator
   (+) = f1
 
-  (*) :: Nat -> Nat -> Nat --Define mult operators
+  (*) :: Nat -> Nat -> Nat --Define mult operator
   (*) = f2
-  --required syntax of the instance
+
   abs :: Nat -> Nat
   abs n = n
   signum :: Nat -> Nat
@@ -26,38 +59,41 @@ instance Num Nat where --Define basics algebraic operations
   signum Succ{} = Succ Zero
   negate :: Nat -> Nat
   negate n = n
-instance Arbitrary Nat where --Generate random values
-  --random generation of values depending on n
-  arbitrary = sized $ \n -> genNat n
+instance Arbitrary Natural where
+  arbitrary :: Gen Natural
+  arbitrary = sized $ \n -> genNatural n
     where
-      genNat :: Int -> Gen Nat --Funtion that generate a random value of Nat
-      genNat 0 = return Zero
-      genNat n = oneof [return Zero, Succ <$> genNat (n - 1)]
+      genNatural :: Int -> Gen Natural
+      genNatural 0 = return NatZero
+      genNatural n = oneof [return NatZero, NatSucc <$> genNatural (n - 1)]
+
+  shrink :: Natural -> [Natural]
   --No values are generated because there are no values smaller than zero
-  shrink Zero     = []
+  shrink NatZero = []
   --If the value generated is Succ then it generates smaller values than the previous one
-  shrink (Succ n) = n : shrink n
-instance Ord Nat where -- Compare Nats values to order them
-  Zero <= _ = True
-  Succ m <= Succ n = m <= n
-  _ <= _ = False
+  shrink (NatSucc n) = n : shrink n
+instance Show Natural where
+  show :: Natural -> String
+  show NatZero = "NatZero"
+  show (NatSucc n) = "NatSucc (" ++ show n ++ ")"
 instance Show Nat where --Converts to strings
   show n = show (toInteger n)
 ----Funtiones
 f1 :: Nat -> Nat -> Nat -- Sum
 f1 m n = recNat n (\ _ y -> Succ y) m
 
-f2 :: Nat -> Nat -> Nat --Multiplication
-f2 m = recNat Zero (\_ acc -> f2 acc m)
+f2 :: Nat -> Nat -> Nat -- Multiplication
+f2 m = recNat Zero (\_ acc -> acc + m)
 
 f3 :: Nat -> Nat -> Nat --Pow
 f3 m n = recNat (Succ Zero) (\_ y -> f2 m y) n
 
-f4 :: Nat -> Nat -> Nat -- Sum using the result of f1
-f4 m n = recNat n (\ _ acc -> f1 m acc) m
+f4 :: Nat -> Nat -> Nat --Predecesor
+f4 Zero _ = Zero
+f4 (Succ n) m = recNat m (\_ prev -> prev) n
 
-f5 :: Nat -> Nat -> Nat --Id
-f5 m n = recNat m (\_ acc -> Succ acc) n
+f5 :: Nat -> Nat -> Nat --Ck(x) = k
+f5 k _ = recNat k (\_ acc -> acc) k
 
 recNat :: a -> (Nat -> a -> a) -> Nat -> a
 recNat a _ Zero = a
@@ -66,3 +102,10 @@ recNat a h (Succ n) = h n (recNat a h n)
 toInteger :: Nat -> Integer --Converts to Integer
 toInteger = recNat 0 (\_ acc -> acc + 1)
 
+natToNatural :: Nat -> Natural
+natToNatural Zero = NatZero
+natToNatural (Succ n) = NatSucc (natToNatural n)
+
+naturalToNat :: Natural -> Nat
+naturalToNat NatZero = Zero
+naturalToNat (NatSucc n) = Succ (naturalToNat n)
--- src/Tests.hs
+++ src/Tests.hs
@@ -1,33 +1,46 @@
 import PRF
-import Test.QuickCheck
-  ( Property
-  , quickCheck
-  , (===)
-  , (==>)
-  )
-  --, verboseCheck
+import Test.QuickCheck (quickCheck)
 
---Properties
-prop_f1 :: Nat -> Nat -> Property --Commutative sum
-prop_f1 x y = (x + y) === (y + x)
+prop_f1 :: Natural -> Natural -> Bool--Commutative sum
+prop_f1 m n = natToNatural (f1 m' n') == m + n
+   where
+     m', n' :: Nat
+     m' = naturalToNat m
+     n' = naturalToNat n
 
-prop_f2 :: Nat -> Nat -> Property --commutative mult
-prop_f2 x y = (x * y) === (y * x)
+prop_f2 :: Natural -> Natural -> Bool --conmutative multiplication
+prop_f2 m n = natToNatural (f2 m' n') == m * n
+   where
+     m', n' :: Nat
+     m' = naturalToNat m
+     n' = naturalToNat n
 
-prop_f3 :: Nat -> Nat -> Property --Numbers raised to 0
-prop_f3 x _ = f3 x Zero === Succ Zero
+prop_f3 :: Natural -> Natural -> Bool --compares the result of the function f3 power with one from the same Num Natural scheme but for power
+prop_f3 m n = natToNatural (f3 m' n') == m PRF.^ n
+    where
+     m', n' :: Nat
+     m' = naturalToNat m
+     n' = naturalToNat n
 
-prop_f4 :: Nat -> Nat -> Property --Higher than 0
-prop_f4 m n = (m > Zero && n > Zero) ==> f4 m n > m
+-- prop_f4 :: Natural -> Natural -> Bool
+-- prop_f4 n m = n == expected
+--   where
+--     natN = naturalToNat n
+--     natM = naturalToNat m
+--     expected = natToNatural (f4 natN natM)
+--prop_f4 n m = n == natToNatural (f4 (naturalToNat n) (naturalToNat m))
 
-prop_f5 :: Nat -> Nat -> Property --Id / conmutative
-prop_f5 n = (\m -> f5 n m === f5 m n)
+prop_f5 :: Natural -> Natural -> Bool --compares if the constante funtion works
+prop_f5 m n = natToNatural (f5 m' n') == m
+   where
+     m' = naturalToNat m
+     n' = naturalToNat n
 
 --Main
 main :: IO()
 main = do
-    quickCheck prop_f1
-    quickCheck prop_f2
-    quickCheck prop_f3
-    quickCheck prop_f4
-    quickCheck prop_f5
+      quickCheck prop_f1
+      quickCheck prop_f2
+      quickCheck prop_f3
+      --quickCheck prop_f4
+      quickCheck prop_f5
